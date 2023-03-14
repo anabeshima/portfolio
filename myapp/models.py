3b5892b039232cf36b1bc9c551a275fcc0cad1ea
Ayano Nabeshima <den.lille.havfrue.ahc@gmail.com>
2023-03-14 06:48:34 +0000

    Co-authored-by: ttatewaki@gmail.com <ttatewaki@gmail.com>

---
+++ myapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Question(models.Model):
+    text = models.CharField(max_length=200)
+
+class Choicce(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)

9d90806747735fed0d3f2d66cb2c70e8f996dad0
Ayano Nabeshima <den.lille.havfrue.ahc@gmail.com>
2023-03-16 14:23:19 +0000

    ほぼ完成

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('myapp.urls')),
+    path('', include('portfolio.urls')),
+    path('survey/', include('myapp.urls')),
     path("admin/", admin.site.urls),
 ]

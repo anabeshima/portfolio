33690da87ff1100098a089f8a71ff5f2dcab484f
anabeshima <128060354+anabeshima@users.noreply.github.com>
2023-04-05 11:15:51 +0900

    Add files via upload

---
+++ ayano-python/myapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Question(models.Model):
+    text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    
---
+++ ayano-python/myapp/views.py
@@ -0,0 +1,35 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import render
+from .models import Question, Choice
+
+
+def index(request):
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_questions': latest_questions}
+    return render(request, 'myapp/index.html', context)
+
+
+def detail(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    return render(request, 'myapp/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    return render(request, 'myapp/results.html', {'question': question})
+
+
+def vote(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'myapp/detail.html', {
+            'question': question,
+            'error_message': "回答を選択していません"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('myapp:results', args=(question.id,)))
